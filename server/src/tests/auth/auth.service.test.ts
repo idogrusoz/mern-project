01417968ed06bc7d00428822125667fe156fa85f
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-15 00:39:54 +0200

    REFAC: Minor naming change

--- server/src/tests/auth/auth.service.test.ts
+++ server/src/tests/auth/auth.service.test.ts
@@ -9,26 +9,26 @@ import buildServiceResponse from "../../utils/serviceResponse";
 const { findOne, create } = UserModel;
 
 describe("Auth service tests", () => {
-    it("Hashes a given passport", async () => {
+    it("hashes a given passport", async () => {
         const password = "password";
         const hashed = await hashedPassword(password);
         expect(hashed).toBeTruthy();
         expect(hashed).not.toEqual(password);
     });
-    it("Returns 401 if user exists", async () => {
+    it("returns error if user exists", async () => {
         (findOne as jest.Mock).mockReturnValue(mockUser as UserDocument);
         const expectedResponse = buildServiceResponse(true, 401, "This email is already in use");
         const response: ServiceResponse<UserInterface> = await registerNewUser(mockUser);
         expect(response).toEqual(expectedResponse);
     });
-    it("Returns 200 if user is created", async () => {
+    it("returns success if user is created", async () => {
         (findOne as jest.Mock).mockReturnValue(null);
         (create as jest.Mock).mockReturnValue(mockUser as UserDocument);
         const expectedResponse = buildServiceResponse(false, 200, "", mockUser);
         const response: ServiceResponse<UserInterface> = await registerNewUser(mockUser);
         expect(response).toEqual(expectedResponse);
     });
-    it("Handles creation error", async () => {
+    it("handles creation error", async () => {
         (findOne as jest.Mock).mockReturnValue(null);
         (create as jest.Mock).mockReturnValue(undefined);
         const expectedResponse = buildServiceResponse(true, 500, "An error occured");
