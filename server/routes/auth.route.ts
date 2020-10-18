46723599ca82cbb57de9cd97fd8c3eb756a70bc0
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-19 00:50:30 +0200

    FEAT: Add signout route

--- server/routes/auth.route.ts
+++ server/routes/auth.route.ts
@@ -1,10 +1,10 @@
-import { signIn, register, authenticate } from "./../controllers/auth.controller";
+import { signIn, register, authenticate, signOut } from "./../controllers/auth.controller";
 import { Router } from "express";
 import { verification } from "../middlewares/auth.middleware";
-// import { register } from "../controllers/auth.controller";
 
 const auth: Router = Router();
 
+auth.route("/signout").get(signOut);
 auth.route("/register").post(register);
 auth.route("/signin").post(signIn);
 auth.route("/authenticate").get(verification, authenticate);
