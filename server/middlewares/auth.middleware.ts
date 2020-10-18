3c531bb88a959e1226955f0ef6512f3ef7648c7e
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-19 00:49:16 +0200

    FIX: Response handling

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -6,11 +6,14 @@ import { ServiceResponse } from "../utils/serviceResponse";
 
 export const verification = async (req: Request, res: Response, next: NextFunction) => {
     const token: string = tokenExtractor(req.headers);
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
     const { error, message, statusCode, data }: ServiceResponse<ResponseUser> = await verifyToken(token);
     if (error) {
-        res.send(statusCode).json({ message });
-    } else {
-        req.body.user = data;
-        next();
+        return res.status(statusCode).json({ message });
     }
+    req.body.user = data;
+    req.body.token = token;
+    next();
 };
