6cbbcdb01bfc3e28f5efba2aed1dde399c846d3e
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-19 00:48:51 +0200

    FEAT: Add authentication and signout routes
    - a fix for response handling is implemented too

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -4,30 +4,44 @@ import { BaseUser } from "../interfaces/user.interfaces";
 import { registerNewUser, signInUser, signToken } from "../service/auth.service";
 import { ServiceResponse } from "../utils/serviceResponse";
 
-export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const { error, message, statusCode, data }: ServiceResponse<BaseUser> = await registerNewUser(req.body);
-    if (error) {
-        res.status(statusCode).json({ message });
-    } else {
-        res.status(200).json({ user: data });
+export const register = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    try {
+        const { error, message, statusCode, data }: ServiceResponse<BaseUser> = await registerNewUser(req.body);
+        return res.status(statusCode).json({ error, message, user: data });
+    } catch (error) {
+        return res.status(500).json({ message: "An error occured :" + error.message });
     }
 };
 
-export const signIn = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const { error, message, statusCode, data }: ServiceResponse<ResponseUser> = await signInUser(req.body);
-    if (error) {
-        res.status(statusCode).json({ message });
-    } else {
-        passToken(res, data!);
+export const signIn = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    try {
+        const { error, message, statusCode, data }: ServiceResponse<ResponseUser> = await signInUser(req.body);
+        if (error) {
+            return res.status(statusCode).json({ message });
+        }
+        const token = signToken(data!._id);
+        return res.cookie("access_token", token, { httpOnly: true, sameSite: true }).json({
+            isAuthenticated: true,
+            user: data,
+            token,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "An error occured :" + error.message });
     }
 };
 
-const passToken = (res: Response, user: ResponseUser): void => {
-    const token = signToken(user._id);
-    res.cookie("access_token", token, { httpOnly: true, sameSite: true });
-    res.status(200).json({ isAuthenticated: true, user, token });
+export const authenticate = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    try {
+        return res.status(200).json({ isAuthenticated: true, user: req.body.user, token: req.body.token });
+    } catch (error) {
+        return res.status(500).json({ message: "An error occured :" + error.message });
+    }
 };
 
-export const authenticate = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    passToken(res, req.body.user);
+export const signOut = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    try {
+        return res.cookie("access_token", "", { expires: new Date() }).json({ isAuthenticated: false });
+    } catch (error) {
+        return res.status(500).json({ message: "An error occured :" + error.message });
+    }
 };
