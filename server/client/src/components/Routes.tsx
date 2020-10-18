4c7f2fb7899cc29e5e7cab57449bc9a73aa7e82b
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-19 00:45:27 +0200

    FEAT: Control authentication on mount to redirect

--- server/client/src/components/Routes.tsx
+++ server/client/src/components/Routes.tsx
@@ -1,12 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Route, RouteComponentProps, Router, Switch } from "react-router-dom";
 import { AuthContext } from "./auth/AuthContext";
 import Landing from "./auth/Landing";
 import Profile from "./Profile";
 
 const Routes = ({ history, location }: RouteComponentProps<{}>) => {
-    const { authenticated, user } = useContext(AuthContext);
-    return (
+    const [loading, setLoading] = useState<boolean>(true);
+    const { authenticated, user, isAuthenticated } = useContext(AuthContext);
+    const fetch = () => {
+        isAuthenticated()
+            .then(() => setLoading(false))
+            .catch((err) => console.log("Error: ", err));
+    };
+    useEffect(fetch, []);
+    return !loading ? (
         <Router history={history}>
             {authenticated && user ? (
                 <Switch>
@@ -16,6 +23,8 @@ const Routes = ({ history, location }: RouteComponentProps<{}>) => {
                 <Route exact path="/" component={Landing} />
             )}
         </Router>
+    ) : (
+        <h1>Loading</h1>
     );
 };
 
