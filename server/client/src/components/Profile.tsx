27892a778c64de359b816aef1c45133221397459
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-19 00:44:37 +0200

    CHORE: Add signout button to test

--- server/client/src/components/Profile.tsx
+++ server/client/src/components/Profile.tsx
@@ -1,12 +1,14 @@
+import { Button } from "@material-ui/core";
 import React, { useContext } from "react";
 import { AuthContext } from "./auth/AuthContext";
 
 const Profile = () => {
-    const { user } = useContext(AuthContext);
+    const { user, signOut } = useContext(AuthContext);
     return (
         <div>
             <h1>{user!.displayName}</h1>
             <h3>{user!._id}</h3>
+            <Button onClick={signOut}>Sign Out</Button>
         </div>
     );
 };
