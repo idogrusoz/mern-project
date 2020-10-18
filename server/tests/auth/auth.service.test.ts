3684ad0b761671d1228616c6e0c956be9f10a3af
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-19 00:57:33 +0200

    TEST: Improve test Service and model tests are combined in order to have more credible results and avoid using mocks

--- server/tests/auth/auth.service.test.ts
+++ server/tests/auth/auth.service.test.ts
@@ -1,58 +1,54 @@
-jest.mock("./../../models/user/user.model");
+// jest.mock("./../../models/user/user.model");
 import { signInUser } from "./../../service/auth.service";
-import { UserDocument } from "./../../models/user/user.types";
 import { UserModel } from "./../../models/user/user.model";
-import {
-    mockUser,
-    mockUserWithId,
-    responseUser,
-    userWithCorrectPassword,
-    userWithWrongPassword,
-} from "./../resources/user";
+import { mockUser, userWithCorrectPassword, userWithWrongPassword } from "../test-resources/user";
 import { registerNewUser } from "../../service/auth.service";
 import { BaseUser } from "../../interfaces/user.interfaces";
-import buildServiceResponse, { ServiceResponse } from "../../utils/serviceResponse";
-
-const { findOne, create, findByCredentials } = UserModel;
+import { ServiceResponse } from "../../utils/serviceResponse";
+import mongoose from "../../config/db";
+import { MONGO_URI } from "../jest.setup";
 
 describe("Auth service tests", () => {
-    it("returns error if user exists", async () => {
-        (findOne as jest.Mock).mockReturnValue([mockUser as UserDocument]);
-        const expectedResponse = buildServiceResponse(true, 401, "This email is already in use");
-        const response: ServiceResponse<BaseUser> = await registerNewUser(mockUser);
-        expect(response).toEqual(expectedResponse);
+    let connection: typeof mongoose;
+    beforeAll(async () => {
+        connection = await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+        });
+    });
+
+    afterAll(async () => {
+        await UserModel.deleteMany({});
+        await connection.disconnect();
     });
     it("returns success if user is created", async () => {
-        (findOne as jest.Mock).mockReturnValue(null);
-        (create as jest.Mock).mockReturnValue(mockUser as UserDocument);
-        const expectedResponse = buildServiceResponse(false, 200, "", mockUser);
         const response: ServiceResponse<BaseUser> = await registerNewUser(mockUser);
-        expect(response).toEqual(expectedResponse);
+        expect(response.data?.email).toEqual(mockUser.email);
+    });
+    it("returns error if user exists", async () => {
+        const response: ServiceResponse<BaseUser> = await registerNewUser(mockUser);
+        expect(response.statusCode).toEqual(409);
     });
     it("handles creation error", async () => {
-        (findOne as jest.Mock).mockReturnValue(null);
-        (create as jest.Mock).mockReturnValue(undefined);
-        const expectedResponse = buildServiceResponse(true, 500, "An error occured");
-        const response: ServiceResponse<BaseUser> = await registerNewUser(mockUser);
-        expect(response).toEqual(expectedResponse);
+        const response: ServiceResponse<BaseUser> = await registerNewUser({ ...mockUser, email: "email" });
+        console.log("response", response);
+        expect(response.statusCode).toEqual(400);
     });
     it("returns error if user doesn't exist", async () => {
-        (findByCredentials as jest.Mock).mockReturnValue(null);
-        const expectedResponse = buildServiceResponse(true, 404, "This user doesn't exist");
-        const response: ServiceResponse<BaseUser> = await signInUser(userWithCorrectPassword);
-        expect(response).toEqual(expectedResponse);
+        const response: ServiceResponse<BaseUser> = await signInUser({
+            email: "wrong@email.com",
+            password: "password",
+        });
+        expect(response.statusCode).toEqual(404);
+        expect(response.message).toEqual("This user doesn't exist");
     });
     it("returns error if password is wrong", async () => {
-        (findByCredentials as jest.Mock).mockReturnValue(null);
-        (findOne as jest.Mock).mockReturnValue(mockUser);
-        const expectedResponse = buildServiceResponse(true, 404, "Wrong password");
         const response: ServiceResponse<BaseUser> = await signInUser(userWithWrongPassword);
-        expect(response).toEqual(expectedResponse);
+        expect(response.statusCode).toEqual(404);
+        expect(response.message).toEqual("Wrong password");
     });
     it("returns success if user is signedIn", async () => {
-        (findByCredentials as jest.Mock).mockReturnValue(mockUserWithId as UserDocument);
-        const expectedResponse = buildServiceResponse(false, 200, "", responseUser);
         const response: ServiceResponse<BaseUser> = await signInUser(userWithCorrectPassword);
-        expect(response).toEqual(expectedResponse);
+        expect(response.statusCode).toEqual(200);
+        expect(response.data?.email).toEqual(mockUser.email);
     });
 });
