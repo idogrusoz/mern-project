6c3bf4063cf64633eecb8d25e5312e78917069bf
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-21 23:14:36 +0200

    FIX: Data extraction

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import { ServiceResponse } from "../utils/serviceResponse";
 
 export const checkUserName = async (req: Request, res: Response): Promise<Response> => {
     try {
-        const { error, message, statusCode, data }: ServiceResponse<boolean> = await isUserNameFree(req.body);
+        const { error, message, statusCode, data }: ServiceResponse<boolean> = await isUserNameFree(req.body.value);
         return res.status(statusCode).json({ error, message, data });
     } catch (error) {
         return res.status(500).json({ message: "An error occured :" + error.message });
