2c63c31beb6732342ff0b8ce63179a6ce514e862
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-10-27 10:32:43 +0100

    FEAT: Add "likes" tab to the profile

--- server/client/src/components/Profile/Profile.tsx
+++ server/client/src/components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import { Button, Container, Grid, makeStyles } from "@material-ui/core";
 import React, { useCallback, useContext, useEffect, useState } from "react";
-import { IPostInterface } from "../../../../interfaces/post.interfaces";
+import { BasePostDocument } from "../../../../interfaces/post.interfaces";
 import api from "../../api";
 import { AuthContext } from "../Auth/AuthContext";
 import EmptyProfile from "./EmptyProfile";
@@ -23,14 +23,16 @@ const useStyles = makeStyles((theme) => ({
 const Profile = () => {
     const { user } = useContext(AuthContext);
     const { profileOwner } = useContext(ProfileContext);
-    const [posts, setPosts] = useState<IPostInterface[]>([]);
+    const [feed, setFeed] = useState<BasePostDocument[]>([]);
+    const [posts, setPosts] = useState<BasePostDocument[]>([]);
+    const [likes, setLikes] = useState<BasePostDocument[]>([]);
     const [activeTab, setActiveTab] = useState<"feed" | "posts" | "likes">("feed");
     const classes = useStyles();
 
     const fetchFeed = useCallback(async () => {
         try {
             const result = await api.get(`posts/${user?._id}/feed`);
-            setPosts([...result.data.data]);
+            setFeed([...result.data.data]);
         } catch (error) {
             throw error;
         }
@@ -38,8 +40,6 @@ const Profile = () => {
 
     const fetchUserPosts = useCallback(async () => {
         const id = profileOwner ? profileOwner._id : user?._id;
-        console.log("id", id);
-        console.log("profileOwner.userName", profileOwner?.userName);
         try {
             const result = await api.get(`posts/user/${id}`);
             setPosts([...result.data.data]);
@@ -48,14 +48,41 @@ const Profile = () => {
         }
     }, [profileOwner, user]);
 
+    const fetchLikedPosts = useCallback(async () => {
+        const id = profileOwner ? profileOwner._id : user?._id;
+        try {
+            const result = await api.get(`/posts/likedbyuser/${id}`);
+            setLikes([...result.data.data]);
+        } catch (error) {
+            throw error;
+        }
+    }, [profileOwner, user]);
+
     useEffect(() => {
-        if (activeTab === "feed") {
+        if (!profileOwner) {
             fetchFeed();
         }
-        if (activeTab === "posts") {
-            fetchUserPosts();
+        fetchUserPosts();
+        fetchLikedPosts();
+        if (profileOwner) {
+            setActiveTab("posts");
+        } else {
+            setActiveTab("feed");
         }
-    }, [profileOwner, user, activeTab, fetchFeed, fetchUserPosts]);
+    }, [profileOwner, user, fetchFeed, fetchUserPosts, fetchLikedPosts]);
+
+    const postsToRender = (): BasePostDocument[] => {
+        if (activeTab === "feed") {
+            return feed;
+        }
+        if (activeTab === "posts") {
+            return posts;
+        }
+        if (activeTab === "likes") {
+            return likes;
+        }
+        return [];
+    };
 
     return (
         <Container maxWidth="md" className={classes.container}>
@@ -70,7 +97,10 @@ const Profile = () => {
                         <Grid item xs={4} className={activeTab === "feed" ? classes.active : classes.icon}>
                             <Button
                                 color="primary"
-                                onClick={() => setActiveTab("feed")}
+                                onClick={() => {
+                                    fetchFeed();
+                                    setActiveTab("feed");
+                                }}
                                 style={activeTab === "feed" ? { color: "#ffffff" } : {}}
                                 fullWidth
                             >
@@ -85,7 +115,10 @@ const Profile = () => {
                     >
                         <Button
                             color="primary"
-                            onClick={() => setActiveTab("posts")}
+                            onClick={() => {
+                                fetchUserPosts();
+                                setActiveTab("posts");
+                            }}
                             style={activeTab === "posts" ? { color: "#ffffff" } : {}}
                             fullWidth
                         >
@@ -109,7 +142,7 @@ const Profile = () => {
                 </Grid>
                 {posts.length > 0 ? (
                     <Grid item>
-                        <PostsContainer posts={posts} />
+                        <PostsContainer posts={postsToRender()} fetch={fetchLikedPosts} />
                     </Grid>
                 ) : (
                     <EmptyProfile />
