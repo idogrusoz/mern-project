82aeb6c43375e9f7f2ef0b84edfd2aa3878bd917
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-11-01 16:55:08 +0100

    REFAc: Improve logic on mount and further render

--- server/client/src/components/Profile/Profile.tsx
+++ server/client/src/components/Profile/Profile.tsx
@@ -1,10 +1,11 @@
-import { Button, Container, Grid, makeStyles } from "@material-ui/core";
+import { Container, Grid, makeStyles } from "@material-ui/core";
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BasePostDocument } from "../../../../interfaces/post.interfaces";
 import api from "../../api";
 import { AuthContext } from "../Auth/AuthContext";
 import PostsContainer from "../Post/PostsContainer";
+import PostTabs from "./PostTabs";
 import { ProfileContext } from "./ProfileContext";
 import ProfileOwner from "./ProfileOwner";
 
@@ -31,19 +32,23 @@ const Profile = () => {
     const [likes, setLikes] = useState<BasePostDocument[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [activeTab, setActiveTab] = useState<"feed" | "posts" | "likes">("feed");
+    const [postsToRender, setPostsToRender] = useState<BasePostDocument[]>([]);
+    const [displayedId, setDisplayedId] = useState<string | null>(null);
     const classes = useStyles();
-    const location = useLocation();
+    const { pathname } = useLocation();
     const fetchFeed = useCallback(async () => {
-        setIsLoading(true);
-        try {
-            const result = await api.get(`posts/${user?._id}/feed`);
-            setFeed([...result.data.data]);
-            setIsLoading(false);
-        } catch (error) {
-            setIsLoading(false);
-            throw error;
+        if (!profileOwner) {
+            setIsLoading(true);
+            try {
+                const result = await api.get(`posts/${user?._id}/feed`);
+                setFeed([...result.data.data]);
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                throw error;
+            }
         }
-    }, [user]);
+    }, [user, profileOwner]);
 
     const fetchUserPosts = useCallback(async () => {
         setIsLoading(true);
@@ -83,7 +88,6 @@ const Profile = () => {
     const getProfile = async (id: string): Promise<void> => {
         try {
             const response = await api.get(`/users/${id}`);
-            console.log("response", response);
             setProfileOwner(response.data.data);
         } catch (error) {
             throw error;
@@ -94,34 +98,35 @@ const Profile = () => {
         if (id === user?._id) {
             setProfileOwner(null);
         } else {
-            console.log("id", id);
             getProfile(id);
         }
     };
 
+    const updatePosts = useCallback(() => {
+        if (activeTab === "feed") {
+            setPostsToRender(feed);
+        }
+        if (activeTab === "posts") {
+            setPostsToRender(posts);
+        }
+        if (activeTab === "likes") {
+            setPostsToRender(likes);
+        }
+    }, [activeTab, feed, posts, likes]);
+
     useEffect(() => {
-        let id: string = getProfileId(location.pathname);
-        if (id !== "" && id !== profileOwner?._id) {
-            console.log("id", id);
-            profileToRender(id);
+        let id: string = getProfileId(pathname);
+        if (id !== displayedId) {
+            setDisplayedId(id);
+            if (id !== "" && id !== profileOwner?._id) {
+                profileToRender(id);
+            }
         }
         if (profileOwner) {
             setActiveTab("posts");
         }
-    }, [profileOwner, location]);
-
-    const postsToRender = (): BasePostDocument[] => {
-        if (activeTab === "feed") {
-            return feed;
-        }
-        if (activeTab === "posts") {
-            return posts;
-        }
-        if (activeTab === "likes") {
-            return likes;
-        }
-        return [];
-    };
+        updatePosts();
+    }, [profileOwner, location, activeTab, updatePosts, displayedId]);
 
     return (
         <Container maxWidth="md" className={classes.container}>
@@ -131,54 +136,9 @@ const Profile = () => {
                         <ProfileOwner />
                     </div>
                 </Grid>
-                <Grid container direction="row">
-                    {!profileOwner && (
-                        <Grid item xs={4} className={activeTab === "feed" ? classes.activeTab : classes.icon}>
-                            <Button
-                                color="primary"
-                                onClick={() => {
-                                    setActiveTab("feed");
-                                }}
-                                className={activeTab === "feed" ? classes.activeButton : undefined}
-                                fullWidth
-                            >
-                                Feed
-                            </Button>
-                        </Grid>
-                    )}
-                    <Grid
-                        item
-                        xs={profileOwner ? 6 : 4}
-                        className={activeTab === "posts" ? classes.activeTab : classes.icon}
-                    >
-                        <Button
-                            color="primary"
-                            onClick={() => {
-                                setActiveTab("posts");
-                            }}
-                            className={activeTab === "posts" ? classes.activeButton : undefined}
-                            fullWidth
-                        >
-                            Posts
-                        </Button>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={profileOwner ? 6 : 4}
-                        className={activeTab === "likes" ? classes.activeTab : classes.icon}
-                    >
-                        <Button
-                            color="primary"
-                            onClick={() => setActiveTab("likes")}
-                            className={activeTab === "likes" ? classes.activeButton : undefined}
-                            fullWidth
-                        >
-                            Likes
-                        </Button>
-                    </Grid>
-                </Grid>
+                <PostTabs activeTab={activeTab} setActiveTab={setActiveTab} />
                 <PostsContainer
-                    posts={postsToRender()}
+                    posts={postsToRender}
                     fetchLikedPosts={fetchLikedPosts}
                     activeTab={activeTab}
                     fetchFeed={fetchFeed}
