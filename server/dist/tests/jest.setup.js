e6ce450545741fc12d00716cb2d6997e10877572
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-11-03 15:22:34 +0100

    CHORE:Remove compiled js code

--- server/dist/tests/jest.setup.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URI = void 0;
-exports.MONGO_URI = "mongodb+srv://idogrusoz:253Ott3zq7MGVNSk@cluster0.tza8i.mongodb.net/test";
