cd8ccc04bd1f164a682cccb657f55532eb627ee5
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2020-11-05 12:22:46 +0100

    FIX: API route in production

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? process.env.PUBLIC_URL : `http://localhost:5000/api/v1/`,
+    baseURL:
+        process.env.NODE_ENV === "production" ? process.env.PUBLIC_URL + "/api/v1/" : `http://localhost:5000/api/v1/`,
     withCredentials: true,
 });
